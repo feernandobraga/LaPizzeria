79b3af0b2e4b11d44f248e0cb6ac28dbbb75c724
Fernando <fernando.bragga@gmail.com>
2021-02-25 20:18:37 +1100

    add form

--- src/pages/OrderPage/index.js
+++ src/pages/OrderPage/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PageHeader from "../../components/PageHeader";
 import PizzaCard from "../../components/PizzaCard";
 import margheritaImg from "../../assets/images/margherita.jpg";
@@ -6,10 +6,30 @@ import bbqBeefImg from "../../assets/images/bbq-beef.jpg";
 import prawnImg from "../../assets/images/prawn.jpg";
 import chickenBaconImg from "../../assets/images/smoky-chicken-bacon.jpg";
 import "./styles.css";
-import { Grid } from "@material-ui/core";
+import {
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@material-ui/core";
 
 const OrderPage = () => {
   const [selectedPizza, setSelectedPizza] = useState("");
+  const [formData, setFormData] = useState({
+    quantity: 1,
+    size: "Medium",
+    crust: "Stuffed",
+  });
+  const [formError, setFormError] = useState("");
+  const [pizzaSize, setPizzaSite] = useState("Small");
+  // const [formHasErrors, setFormHasErrors] = useState(false);
+
+  useEffect(() => {
+    console.log(formData);
+  }, [formData]);
 
   const margherita = {
     title: "Margherita",
@@ -43,10 +63,30 @@ const OrderPage = () => {
 
   const availablePizzas = [margherita, bbqBeef, prawn, chickenBacon];
 
-  function handleSelectPizza(pizza) {
+  const handleSelectPizza = (pizza) => {
     setSelectedPizza(pizza);
-    console.log(`Pizza selected ${pizza}`);
-  }
+    // console.log(`Pizza selected ${pizza}`);
+  };
+
+  const handleOnChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (selectedPizza === "") {
+      setFormError("You must select a Pizza");
+    } else {
+      setFormError("");
+      setFormData({
+        ...formData,
+        selectedPizza,
+      });
+    }
+  };
 
   return (
     <>
@@ -70,6 +110,115 @@ const OrderPage = () => {
           })}
         </Grid>
       </main>
+
+      <article>
+        <form onSubmit={handleFormSubmit}>
+          <Grid container spacing={3} justify="center">
+            <Grid item xs={12} md={4}>
+              <TextField
+                id="pizza-quantity"
+                fullWidth
+                label="Quantity"
+                variant="outlined"
+                name="quantity"
+                value={formData.quantity}
+                type="number"
+                onChange={handleOnChange}
+                // required
+              />
+            </Grid>
+
+            <Grid item xs={12} md={4}>
+              <FormControl variant="outlined" fullWidth>
+                <InputLabel id="pizza-size">Size</InputLabel>
+                <Select
+                  labelId="pizza-size"
+                  name="size"
+                  value={formData.size}
+                  onChange={handleOnChange}
+                  label="Size"
+                >
+                  <MenuItem value={"Small"}>Small</MenuItem>
+                  <MenuItem value={"Medium"}>Medium</MenuItem>
+                  <MenuItem value={"Large"}>Large</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12} md={4}>
+              <FormControl variant="outlined" fullWidth>
+                <InputLabel id="pizza-crust">Crust</InputLabel>
+                <Select
+                  labelId="pizza-crust"
+                  name="crust"
+                  value={formData.crust}
+                  onChange={handleOnChange}
+                  label="Size"
+                >
+                  <MenuItem value={"Traditional"}>Traditional</MenuItem>
+                  <MenuItem value={"Stuffed"}>Stuffed</MenuItem>
+                  <MenuItem value={"GlutenFree"}>Gluten Free</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                // id="client-name"
+                fullWidth
+                label="Client Name"
+                variant="outlined"
+                name="clientName"
+                // required
+                // onChange={handleOnChange}
+                // error={!!formErrors.clientName}
+                // helperText={formErrors.clientName}
+                // onBlur={validateForm}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                // id="outlined-basic"
+                fullWidth
+                label="Address"
+                variant="outlined"
+                name="Address"
+                // required
+                // onChange={handleOnChange}
+                // error={!!formErrors.clientName}
+                // helperText={formErrors.clientName}
+                // onBlur={validateForm}
+              />
+            </Grid>
+
+            <Grid item sm={12} md={6}>
+              <Button
+                variant="outlined"
+                // color="tertiary"
+                type="submit"
+                fullWidth
+                id="submit-button"
+                size="large"
+              >
+                Submit
+              </Button>
+            </Grid>
+            <Grid item sm={12} md={6}>
+              <Button
+                variant="outlined"
+                color="secondary"
+                type="submit"
+                fullWidth
+                size="large"
+              >
+                Cancel
+              </Button>
+            </Grid>
+            {formError ? <small className="form-error">{formError}</small> : null}
+          </Grid>
+        </form>
+      </article>
     </>
   );
 };
